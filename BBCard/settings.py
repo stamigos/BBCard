5841e62ece029a9f2e5c45d4b8abf87429471274
Amigos <stamigos@plasticjam.com>
2014-10-06 00:10:02 +0300

    changed db port

--- BBCard/settings.py
+++ BBCard/settings.py
@@ -17,12 +17,12 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(BASE_DIR,'stamigos_bbcard'),                      # Or path to database file if using sqlite3.
+        'NAME': 'stamigos_bbcard',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'stamigos_django',
         'PASSWORD': 'elenamelnik',
         'HOST': '10.0.0.2',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '3306',                      # Set to empty string for default.
+        'PORT': '',                      # Set to empty string for default.
     }
 }
 
