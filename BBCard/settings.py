75a50dc186f4f313a31ed69cbdb07377bb127085
Amigos <stamigos@plasticjam.com>
2014-10-05 17:20:00 +0300

    исправлены параметры доступа к mysql

--- BBCard/settings.py
+++ BBCard/settings.py
@@ -17,9 +17,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(BASE_DIR,'stamigos_django.db'),                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(BASE_DIR,'stamigos_bbcard.db'),                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'stamigos_bbcard',
+        'USER': 'stamigos_django',
         'PASSWORD': 'elenamelnik',
         'HOST': '10.0.0.2',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
