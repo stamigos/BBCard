80920a12a86af24bb1d333cabadbc2f00d7638ee
Amigos <stamigos@plasticjam.com>
2014-10-05 16:54:02 +0300

    added hosts to allow

--- BBCard/settings.py
+++ BBCard/settings.py
@@ -9,7 +9,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    ('amigos', 'amigos@example.com'),
+#    ('amigos', 'amigos@example.com'),
 )
 
 MANAGERS = ADMINS
@@ -28,7 +28,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['stamigos.djangohost.name']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
