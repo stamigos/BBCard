d693aa00c6682a79fed752c25787a9652d06042d
Amigos <stamigos@plasticjam.com>
2014-10-05 17:00:32 +0300

    настроен wsgi

--- BBCard/wsgi.py
+++ BBCard/wsgi.py
@@ -13,20 +13,33 @@ middleware here, or combine a Django application with an application of another
 framework.
 
 """
-import os
+#import os
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "BBCard.settings"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BBCard.settings")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BBCard.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+#from django.core.wsgi import get_wsgi_application
+#application = get_wsgi_application()
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
+activate_this = '/domains/stamigos.djangohost.name/public_html/BBCard/bbc_env/bin/activate_this.py'
+execfile(activate_this, dict(__file__=activate_this))
+
+import os
+import sys
+
+path = '/domains/stamigos.djangohost.name/public_html/BBCard'
+if path not in sys.path:
+    sys.path.append(path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'BBCard.settings'
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
