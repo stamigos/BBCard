3c962f581fad5242f1e34062e38e7cf4546cdd20
Amigos <stamigos@plasticjam.com>
2014-10-05 23:58:04 +0300

    changed db name to correct

--- BBCard/settings.py
+++ BBCard/settings.py
@@ -17,7 +17,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(BASE_DIR,'stamigos_bbcard.db'),                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(BASE_DIR,'stamigos_bbcard'),                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'stamigos_django',
         'PASSWORD': 'elenamelnik',
