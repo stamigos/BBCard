86c2ed1d64e3a611bef6c253634840b2ab3eb1e5
Amigos <stamigos@plasticjam.com>
2014-10-07 15:41:03 +0300

    Добавлено теги и категории

--- app/models.py
+++ app/models.py
@@ -3,6 +3,13 @@ from django.db import models
 
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     subtitle = models.CharField(max_length=255)
@@ -10,19 +17,12 @@ class Post(models.Model):
     content = models.TextField(max_length=10000)
     category = models.ForeignKey(Category)
     tag = models.ManyToManyField(Tag)
-    
+
     def __unicode__(self):
         return self.title
 
     def get_absolute_url(self):
         return "/%i/" % self.id
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __unicode__(self):
-        return self.name
 
 
 class Tag(models.Model):
