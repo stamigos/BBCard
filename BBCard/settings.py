9136a68968a2500a998d630273bc8dbb4be50e9e
Amigos <stamigos@plasticjam.com>
2014-10-09 19:09:09 +0300

    Поддержка IE

--- BBCard/settings.py
+++ BBCard/settings.py
@@ -16,13 +16,14 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'stamigos_bbcard',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(BASE_DIR, 'bbcard.db'),
+#        'NAME': 'stamigos_bbcard',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'stamigos_django',
-        'PASSWORD': 'elenamelnik',
-        'HOST': '10.0.0.2',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+#        'USER': 'stamigos_django',
+#        'PASSWORD': 'elenamelnik',
+#        'HOST': '10.0.0.2',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+#        'PORT': '',                      # Set to empty string for default.
     }
 }
 
