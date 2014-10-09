a6cb213b2aff7dbeaeeb3185425cbbe5db47af5c
Amigos <stamigos@plasticjam.com>
2014-10-09 19:38:15 +0300

    Поддержка IE

--- app/urls.py
+++ app/urls.py
@@ -6,10 +6,10 @@ from app.views import PostsListView, PostDetailView
 
 urlpatterns = patterns('',
 
-url(r'^$', PostsListView.as_view(), name='list'),
+#url(r'^$', PostsListView.as_view(), name='list'),
 url(r'^(?P<pk>\d+)/$', PostDetailView.as_view(), name='post'),
 url(r'^news/$', PostsListView.as_view(), name='news'),
-url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
+url(r'^$', TemplateView.as_view(template_name='about.html'), name='about'),
 url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
 url(r'^services/$', TemplateView.as_view(template_name='services.html'), name='services'),
 url(r'^(\d+)/category/(?P<id>\d+)/$', 'app.views.category'),
