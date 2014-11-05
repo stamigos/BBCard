d38eef765d83bd828ddcbc23bb226f25b4036343
stamigos <oldtigersvoice@gmail.com>
2014-11-06 00:32:04 +0200

    наполнение сайта контентом

--- app/urls.py
+++ app/urls.py
@@ -12,6 +12,8 @@ url(r'^news/$', PostsListView.as_view(), name='news'),
 url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
 url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
 url(r'^services/$', TemplateView.as_view(template_name='services.html'), name='services'),
+url(r'^services/service1/$', TemplateView.as_view(template_name='service1.html'), name='service1'),
+url(r'^services/service2/$', TemplateView.as_view(template_name='service2.html'), name='service2'),
 url(r'^(\d+)/category/(?P<id>\d+)/$', 'app.views.category'),
 url(r'^(\d+)/tag/(?P<id>\d+)/$', 'app.views.tag'),
 )
